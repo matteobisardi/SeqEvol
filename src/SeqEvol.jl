6b28af3616e423d58fe3175b177dd0830b609e0c
Matteo Bisardi <matteo.bisardi@gmail.com>
2021-09-10 12:48:37 +0200

    included new code

--- src/SeqEvol.jl
+++ src/SeqEvol.jl
@@ -1,11 +1,15 @@
 module SeqEvol
 
+export fasta2matrix
+
 using DelimitedFiles 
 using FastaIO 
 using LinearAlgebra 
 using StatsBase
 
 include("read_write.jl")
-inclide("evol.jl")
+include("evol.jl")
+
+
 
 end # module
