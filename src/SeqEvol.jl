1e0678fe641b8a110a22be6c1ee25708447b5c77
Matteo Bisardi <matteo.bisardi@gmail.com>
2021-09-10 13:11:42 +0200

    small changes

--- src/SeqEvol.jl
+++ src/SeqEvol.jl
@@ -1,6 +1,11 @@
 module SeqEvol
 
-export fasta2matrix
+export 	fasta2matrix, 
+		read_par_BM, 
+		symmetrize_J, 
+		set_max_field_to_0, 
+		evol_msa_fix_steps_DNA_gibbs, 
+		evol_seq_fix_steps_DNA_gibbs
 
 using DelimitedFiles 
 using FastaIO 
@@ -8,7 +13,7 @@ using LinearAlgebra
 using StatsBase
 
 include("read_write.jl")
-include("evol.jl")
+inclugit switch de("evol.jl")
 
 
 
