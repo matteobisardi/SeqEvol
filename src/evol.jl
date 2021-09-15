05ac37c9c3bbdaefe2ceb96aca3d3e7f0be15812
Matteo Bisardi <matteo.bisardi@gmail.com>
2021-09-15 18:00:48 +0200

    Changed Float64 to Real

--- src/evol.jl
+++ src/evol.jl
@@ -87,7 +87,7 @@ end
 #MAIN FUNCTION
 ####
 
-function evolMSA(output_path::AbstractString, params_path::AbstractString, wt_path::AbstractString; steps::Integer = 10, nseq::Integer = 100, T::Float64 = 1, wt_name = "unknown wt") 
+function evolMSA(output_path::AbstractString, params_path::AbstractString, wt_path::AbstractString; steps::Integer = 10, nseq::Integer = 100, T::Real = 1, wt_name = "unknown wt") 
 	h, J = extract_params(params_path)
 	DNA_seq = readdlm(wt_path)[:, 1]
 	amino_seq = [cod2amino[codon] for codon in DNA_seq]
@@ -102,7 +102,7 @@ function evolMSA(output_path::AbstractString, params_path::AbstractString, wt_pa
 end
 
 
-function evolMSA(output_path::AbstractString, params::Tuple{Array{Float64, 2}, Array{Float64, 4}}, wt_path::AbstractString; steps::Integer = 10, nseq::Integer = 100, T::Float64 = 1, wt_name::AbstractString = "unknown wt") 
+function evolMSA(output_path::AbstractString, params::Tuple{Array{Float64, 2}, Array{Float64, 4}}, wt_path::AbstractString; steps::Integer = 10, nseq::Integer = 100, T::Real = 1, wt_name::AbstractString = "unknown wt") 
 	h, J = params
 	DNA_seq = readdlm(wt_path)[:, 1]
 	amino_seq = [cod2amino[codon] for codon in DNA_seq]
