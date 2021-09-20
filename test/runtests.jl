6b3a11e23f061cdb16197192466ea05ab229a970
Matteo Bisardi <matteo.bisardi@gmail.com>
2021-09-20 14:03:13 +0200

    testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,17 +4,17 @@ using FastaIO
 using Test
 
 ## import test sequence
-seq_test = SeqEvol.fasta2matrix("../data/alitest/seq_test.fasta")[1, :]
+seq_test = SeqEvol.fasta2matrix("../data/alitest/test_seq.fasta")[1, :]
 
 ## import wildtype AAC6
-DNA_seq  = readdlm("../data/wt/AAC6_pfam.fa")[:, 1]
+DNA_seq  = readdlm("../data/wt/AAC6_cod")[:, 1]
 amino_seq = [cod2amino[codon] for codon in DNA_seq]
-seed_seq = SeqToEvolve(amino_seq, DNA_seq)
+seed_seq = SeqEvol.SeqToEvolve(amino_seq, DNA_seq)
 
 ## run the code
-h, J = extract_params("../data/params/params_BM_acetyltransf_1.dat")
+h, J = SeqEvol.extract_params("../data/params/params_BM_acetyltransf_1.dat")
 Random.seed!(2021)
-seq = evol_seq_fix_steps_DNA_gibbs(seed_seq, 100, h, J, 117)
+seq = SeqEvol.evol_seq_fix_steps_DNA_gibbs(seed_seq, 100, h, J, 117)
 
 
 @test seq == seq_test
